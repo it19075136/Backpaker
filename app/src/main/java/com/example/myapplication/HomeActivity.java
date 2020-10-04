7e5ab6e06ba7cfd89b97f837a9bdf677c24f6b1d
it19075204 <it19075204@my.sliit.lk>
2020-10-04 22:59:11 +0530

    adding all

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+    Button btnLogOut;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnLogOut = findViewById(R.id.buttonlogout);
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(HomeActivity.this,logIn.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
