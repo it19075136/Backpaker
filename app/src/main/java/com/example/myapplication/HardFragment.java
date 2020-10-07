25b3325abb1f02c32941651a7d9ce8a77de0b9aa
Pasindu <sandungwp@gmail.com>
2020-10-07 13:30:01 +0530

    edit

--- app/src/main/java/com/example/myapplication/HardFragment.java
+++ app/src/main/java/com/example/myapplication/HardFragment.java
@@ -3,88 +3,143 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HardFragment extends Fragment {
-//    Button hardAddLoca;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public  class HardFragment extends Fragment {
+    private FloatingActionButton hardAddLoca;
     View view;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HardFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HardFragment newInstance(String param1, String param2) {
-        HardFragment fragment = new HardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    GridView gridView;
+    DatabaseReference dbRef;
+    Location location;
+    List<String> locationNames;
+//    String[] loc = {
+//            "Kandy",
+//            "Colombo"
+//    };
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_hard, container, false);
-//    }
-
-@Override
-public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        view =  inflater.inflate(R.layout.fragment_hard, container, false);
-//    hardAddLoca = view.findViewById(R.id.hard_layout);
-//    hardAddLoca.setOnClickListener(new View.OnClickListener() {
+        view  =  inflater.inflate(R.layout.fragment_hard, container, false);
+
+        locationNames = new ArrayList<>();
+        gridView = view.findViewById(R.id.hardlocview);
+
+        location = new Location();
+
+        Query query = FirebaseDatabase.getInstance().getReference("location").orderByChild("type").equalTo("hard");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot childSnap:dataSnapshot.getChildren()){
+                    String locationName = childSnap.child("locationName").getValue(String.class);
+                    locationNames.add(locationName);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        gridView.setAdapter(customAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getContext(),LocationDetail.class);
+                intent.putExtra("locName", locationNames.get(i));
+                startActivity(intent);
+            }
+        });
+//        easyAddLoca = view.findViewById(R.id.easy_floBtn);
+//        easyAddLoca.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
 ////                dbRef = FirebaseDatabase.getInstance().getReference().child("locations/");
 ////                if (TextUtils.isEmpty(txtName.getText().toString())){
 ////                    Toast.makeText(getApplicationContext(), "Empty Name", Toast.LENGTH_SHORT).show();
 ////                }
-//                Intent intent= new Intent(view.getContext(),AddTravelLocationActivity.class);
+////                Intent intent = new Intent();
+//                Intent intent= new Intent(getActivity(),AddTravelLocationActivity.class);
+//                startActivity(intent);
+//            }
+
+//        location.setLocationName("LocationName");
+//        location.setRoute("route");
+//        location.setNote("note");
+//        location.setAccommondation("accommondation");
+//        location.setNOofDays("NOofDays");
+//        location.setRoadCondition("roadCondition");
+//        location.setWeather("Weather");
+//        location.setPermission("permission");
+//        });
+//        clickLocation.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getActivity(),LocationDetail.class);
+////                intent.putExtra("locationName",location.getLocationName());
+//                //Is i want to put other attribties her also
 //                startActivity(intent);
 //            }
 //        });
 
         return view;
     }
+
+    private class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return locationNames.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View view1 = getLayoutInflater().inflate(R.layout.row_data,null);
+            TextView name= view1.findViewById(R.id.name);
+            ImageView image =view1.findViewById(R.id.image);
+            name.setText(locationNames.get(i));
+//            image.setImageResource(locations.get(i).);
+            return view1;
+        }
+    }
 }
+
